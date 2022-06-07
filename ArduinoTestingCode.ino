2f16173826b7783ed8fbd28fdce477de6351b437
4060269 <98860251+4060269@users.noreply.github.com>
2022-06-07 12:47:43 +1000

    Add files via upload

--- ArduinoTestingCode.ino
+++ ArduinoTestingCode.ino
@@ -1,36 +1,31 @@
-/* Global stuff 
- *  SD card works, 50
- *  GPS works, RX to 17, TX to 16
- *  Lights works, A1=Red, A2=Orange, A3=Green
- *  Piezo - works, 48
- *  Servo Motor - 
- *  DC Motor - 
- *  Sonar - 
- *  Line - 
- *  Potetiometer - 
- *  Button 1(big) - 
- *  Button 2(closer to battery pack) - 
- *  Button 3 -
- *  
+/*
+    Global stuff
+    SD card - works, 50
+    GPS - works, RX to 17, TX to 16
+    Lights - works, A1=Red, A2=Orange, A3=Green
+    Piezo - works, 48
+    Servo Motor - works, 6
+    DC Motor - ???
+    Sonar - works, Trig 3, Echo 4
+    Line - works, 7
+    Potetiometer - works, A0
+    Button 1(Crash) - works, 8
+    Button 2(closer to battery pack) - works, 9
+    Button 3 - works, 10
 */
 
 // DC Motor & Motor Module - L298N
 #include <L298N.h>
 
-// Pin definition
-const unsigned int IN3 = 22;
-const unsigned int IN4 = 23;
-
 // Create one motor instance
-L298N motor(IN3, IN4);
+L298N motor(11, 12);
 
 void setup() {
   // DC Motor & Motor Module - L298N
-    motor.setSpeed(70);
-    motor.forward();
+  motor.setSpeed(70);
+  motor.forward();
 }
 
 
-void loop(){
-  
+void loop() {
 }
